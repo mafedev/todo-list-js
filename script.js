19e5f01dffadc7ec5ffbb9d13943a8c67c42cf57
mafedev <ladrillito966@gmail.com>
2025-06-21 19:31:39 +0200

    feat: delete tasks from the list using the delete button

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const lista = document.getElementById("lista");
 
 // Escucha cuando se hace click, para luego tomar ese valor
-document.getElementById("botonAgregar").addEventListener("click", () => {
-  const input = document.getElementById("input").value;
+document.getElementById("boton-agregar").addEventListener("click", () => {
+  const input = document.getElementById("input");
+  const inputValue = input.value.trim(); // Para que no acepte espacios en blanco
+  if (inputValue === "") return;
 
   const tarea = document.createElement("li");
-  tarea.textContent = input;
+  const texto = document.createTextNode(inputValue); // nodo de texto
 
+  // Botón para eliminar la tarea
+  const botonEliminar = document.createElement("button");
+  botonEliminar.textContent = "-";
+  botonEliminar.className = "btn-eliminar";
+
+  botonEliminar.addEventListener("click", () => {
+    lista.removeChild(tarea);
+  });
+
+  // Se agregan los elementos
+  tarea.appendChild(botonEliminar);
+  tarea.appendChild(texto);
   lista.appendChild(tarea);
+
+  input.value = ""
 });
